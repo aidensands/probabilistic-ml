d2b6844b86025a54f1419d79852459b48d6b0521
24sandsa <88sandsa@gmail.com>
2026-04-20 21:43:14 -0400

    Added a beta bernoulli model

---
+++ betabernoulli.py
@@ -0,0 +1,27 @@
+import numpyro
+import numpyro.distributions as dist
+from numpyro.infer import MCMC, NUTS
+import jax.numpy as jnp
+import jax.random as random
+
+
+key = random.PRNGKey(42)
+data = jnp.concat([jnp.ones(50), jnp.zeros(50)])
+data = random.permutation(key, data)
+print(len(data))
+
+
+
+def Model(observations):
+
+    # Prior on the probability of getting heads
+    heads = numpyro.sample('heads', dist.Beta(1, 1))
+
+    with numpyro.plate('data', len(observations)):
+        numpyro.sample('obs', dist.Bernoulli(heads), obs=observations)
+
+
+kernel = NUTS(model=Model)
+mcmc = MCMC(kernel, num_warmup=500, num_samples=1000)
+mcmc.run(key, data)
+mcmc.print_summary()
